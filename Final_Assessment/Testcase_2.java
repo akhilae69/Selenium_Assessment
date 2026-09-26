0d8a6131781c574671c3b387c124c76a3a4ea439
Akhila Reddy <167954687+akhilae69@users.noreply.github.com>
2026-09-26 22:43:25 +0530

    Add TestCase2 for slider interaction using Selenium
    This Java class implements a Selenium test case that interacts with a slider on a web page and checks for the presence of a specific element.

---
+++ Final_Assessment/Testcase_2.java
@@ -0,0 +1,35 @@
+package Final_Assessmnet;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class TestCase2 {
+	public static void main(String[] args) 	 throws InterruptedException{
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://demoapps.qspiders.com/ui/slider?sublist=0");
+		WebElement slider = driver.findElement(By.xpath("//input[@id='slide']"));
+		Actions actions = new Actions(driver);
+		actions.clickAndHold(slider).moveByOffset(200, 0).release().perform();
+		String ele=driver.findElement(By.xpath("//h3[text()='Mens Cotton Jacket']")).getText();
+		if(ele.contains("Mens Cotton Jacket"))
+		{
+			System.out.println("Mens Cotton Jacket is identified");
+		}
+		else
+		{
+			System.out.println("Mens Cotton Jacket not identified");
+
+		}
+		Thread.sleep(4000);
+		driver.quit();
+	}
+
+}
